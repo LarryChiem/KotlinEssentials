639c9816a62e5b2a167cab51844d0dd7000c58bc
Larry Chiem <clarry@pdx.edu>
2020-06-09 22:33:39 -0700

    How statements differ from expressions
    Signed-off-by: Larry Chiem <clarry@pdx.edu>

---
+++ src/statements.kt
@@ -0,0 +1,28 @@
+fun returnsNothing() {
+    println("returnsNothing() function")
+}
+
+fun main() {
+    val bigger = 1000
+    val smaller = 1
+    // won't compile
+//     val max = bigger > smaller? bigger: smaller
+    val max = if(bigger > smaller) bigger else smaller
+
+    val age = 35
+    val boardGroup = when (age) {
+        in 0..18 -> "family"
+        in 65..200 ->"senior"
+        else -> "regular"
+    }
+    println("You are in the $boardGroup group")
+
+    val fromNothing  = returnsNothing()
+    println("$fromNothing")
+
+    val a = 100
+    val b = 101
+    //won't compile
+    // a = b = 98
+    //kotlin doesn't allow multiple statements
+}
